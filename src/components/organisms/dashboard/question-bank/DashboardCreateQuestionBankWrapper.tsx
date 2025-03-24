789dd62d1e3428b3f6ceaedc621405e9538abf71
brilliahib <brilliahib21@gmail.com>
2025-03-25 00:36:24 +0700

    feat: create new question bank

---
+++ src/components/organisms/dashboard/question-bank/DashboardCreateQuestionBankWrapper.tsx
@@ -0,0 +1,5 @@
+import FormCreateQuestionBank from "@/components/molecules/form/question-banks/FormCreateQuestionBank";
+
+export default function DashboardAdminCreateQuestionBankWrapper() {
+  return <FormCreateQuestionBank />;
+}
