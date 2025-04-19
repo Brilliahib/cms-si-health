4b62005f2bf4fca3a6d5a5d6c356387ff55eb0eb
brilliahib <brilliahib21@gmail.com>
2025-04-19 20:35:55 +0700

    feat: score poin on detail test

--- src/components/organisms/dashboard/admin/reports/post-test/DashboardAdminReportDetailPostTestWrapper.tsx
+++ src/components/organisms/dashboard/admin/reports/post-test/DashboardAdminReportDetailPostTestWrapper.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import CardDetailNameOnTest from "@/components/molecules/card/CardDetailNameOnTest";
-import CardListHistoryQuestion from "@/components/molecules/card/CardListHistoryQuestion";
+import CardListHistoryQuestionAdmin from "@/components/molecules/card/CardListHistoryQuestionAdmin";
+import CardScoreHistoryPostTest from "@/components/molecules/card/CardScoreHistoryPostTest";
 import { useGetDetailHistoryPostTest } from "@/http/history/post-test/get-detail-history-post-test";
 import { useSession } from "next-auth/react";
 
@@ -23,7 +24,8 @@ export default function DashboardAdminReportDetailPostTestWrapper({
   return (
     <div className="space-y-4">
       <CardDetailNameOnTest name={data?.data.user.name ?? ""} />
-      <CardListHistoryQuestion data={data?.data} isLoading={isPending} />
+      <CardScoreHistoryPostTest data={data?.data} isLoading={isPending} />
+      <CardListHistoryQuestionAdmin data={data?.data} isLoading={isPending} />
     </div>
   );
 }
