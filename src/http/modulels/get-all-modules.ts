ad7e0a3b142a724c91452f8217ad1823c0a93400
brilliahib <brilliahib21@gmail.com>
2025-04-20 17:13:19 +0700

    fix: get all module by patient type

--- src/http/modulels/get-all-modules.ts
+++ src/http/modulels/get-all-modules.ts
@@ -9,24 +9,29 @@ interface GetAllModulesResponse {
 }
 
 export const GetAllModulesHandler = async (
+  type: string,
   token: string,
 ): Promise<GetAllModulesResponse> => {
-  const { data } = await api.get<GetAllModulesResponse>("/modules", {
-    headers: {
-      Authorization: `Bearer ${token}`,
+  const { data } = await api.get<GetAllModulesResponse>(
+    `/modules?type=${type}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     },
-  });
+  );
 
   return data;
 };
 
 export const useGetAllModules = (
+  type: string,
   token: string,
   options?: Partial<UseQueryOptions<GetAllModulesResponse, AxiosError>>,
 ) => {
   return useQuery({
-    queryKey: ["modules-list"],
-    queryFn: () => GetAllModulesHandler(token),
+    queryKey: ["modules-list", type],
+    queryFn: () => GetAllModulesHandler(type, token),
     ...options,
   });
 };
