a024143b8cfd2be2e9b9524a3a83627073ee66bc
brilliahib <brilliahib21@gmail.com>
2025-04-28 00:31:54 +0700

    feat: background auth and redirect nav link to dashboard

--- src/components/organisms/auth/RegisterWrapperContent.tsx
+++ src/components/organisms/auth/RegisterWrapperContent.tsx
@@ -17,8 +17,10 @@ export default function RegisterWrapperContent() {
         Sign In
       </Link>
       <div className="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-r">
-        <div className="bg-auth-pattern absolute inset-0 bg-cover bg-no-repeat grayscale" />
-        <div className="absolute inset-0 bg-gradient-to-b from-black/60 via-transparent to-black/60" />
+        <div className="absolute inset-0 flex items-center justify-center">
+          <div className="bg-primary bg-auth-pattern absolute inset-0 bg-contain bg-center bg-no-repeat" />
+        </div>
+        <div className="absolute inset-0 bg-gradient-to-b from-black/20 via-transparent to-black/20" />
         <div className="relative z-20 flex items-center gap-x-3 text-lg font-medium">
           <Link href={"/"} className="flex items-center gap-2">
             <Image
