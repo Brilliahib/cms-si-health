f6551c36cd36bb60e58b40a0a984c2b1006f4c3c
brilliahib <brilliahib21@gmail.com>
2025-04-27 22:27:50 +0700

    fix: disabled create new topics discussion when role is users

--- src/components/molecules/card/CardListTopicDiscussion.tsx
+++ src/components/molecules/card/CardListTopicDiscussion.tsx
@@ -5,6 +5,7 @@ import { Discussion } from "@/types/discussions/discussion";
 import { getAvatarColor } from "@/utils/generate-color-avatar";
 import { generateFallbackFromName } from "@/utils/generate-name";
 import { formatRelativeTime } from "@/utils/time-relative";
+import { MessagesSquare } from "lucide-react";
 import Link from "next/link";
 
 interface CardListTopicDiscussionProps {
@@ -42,60 +43,70 @@ export default function CardListTopicDiscussion({
 
   return (
     <div className="flex flex-col gap-4">
-      {data.map((discussion) => (
-        <Link
-          key={discussion.id}
-          href={`/dashboard/discussions/${discussion.id}`}
-        >
-          <Card className="shadow-none">
-            <CardContent>
-              <div className="space-y-4">
-                <h1 className="max-w-xl font-medium break-words whitespace-normal">
-                  {discussion.title}
-                </h1>
-                <div className="flex flex-col gap-2 md:flex-row md:items-center">
-                  {discussion.comments.length > 0 && (
-                    <div className="flex items-center gap-1">
-                      {[
-                        ...new Map(
-                          discussion.comments.map((comment) => [
-                            comment.user.id,
-                            comment,
-                          ]),
-                        ).values(),
-                      ].map((comment) => (
-                        <div key={comment.user.id}>
-                          <Avatar className="h-8 w-8 rounded-full">
-                            <AvatarFallback
-                              className={`rounded-full text-xs font-semibold text-white ${getAvatarColor(comment.user.id)}`}
-                            >
-                              {generateFallbackFromName(comment.user.name)}
-                            </AvatarFallback>
-                          </Avatar>
-                        </div>
-                      ))}
-                    </div>
-                  )}
-                  <p className="text-muted-foreground text-sm">
-                    {discussion.comments.length} Balasan
-                  </p>
-                  {discussion.comments.length > 0 && (
-                    <span className="text-muted-foreground hidden text-xs md:flex">
-                      •
-                    </span>
-                  )}
-                  {discussion.comments.length > 0 && (
-                    <p className="text-muted-foreground hidden text-sm md:flex">
-                      Balasan terakhir{" "}
-                      {formatRelativeTime(discussion.comments[0].created_at)}
+      {data.length === 0 ? (
+        <div className="text-muted-foreground flex flex-col items-center justify-center gap-2 rounded-xl border border-dashed p-6 text-center">
+          <MessagesSquare className="h-10 w-10" />
+          <p className="text-sm">
+            Belum ada topik diskusi! Tunggu atau hubungi admin untuk menambahkan
+            topik diskusi ✨
+          </p>
+        </div>
+      ) : (
+        data.map((discussion) => (
+          <Link
+            key={discussion.id}
+            href={`/dashboard/discussions/${discussion.id}`}
+          >
+            <Card className="shadow-none">
+              <CardContent>
+                <div className="space-y-4">
+                  <h1 className="max-w-xl font-medium break-words whitespace-normal">
+                    {discussion.title}
+                  </h1>
+                  <div className="flex flex-col gap-2 md:flex-row md:items-center">
+                    {discussion.comments.length > 0 && (
+                      <div className="flex items-center gap-1">
+                        {[
+                          ...new Map(
+                            discussion.comments.map((comment) => [
+                              comment.user.id,
+                              comment,
+                            ]),
+                          ).values(),
+                        ].map((comment) => (
+                          <div key={comment.user.id}>
+                            <Avatar className="h-8 w-8 rounded-full">
+                              <AvatarFallback
+                                className={`rounded-full text-xs font-semibold text-white ${getAvatarColor(comment.user.id)}`}
+                              >
+                                {generateFallbackFromName(comment.user.name)}
+                              </AvatarFallback>
+                            </Avatar>
+                          </div>
+                        ))}
+                      </div>
+                    )}
+                    <p className="text-muted-foreground text-sm">
+                      {discussion.comments.length} Balasan
                     </p>
-                  )}
+                    {discussion.comments.length > 0 && (
+                      <span className="text-muted-foreground hidden text-xs md:flex">
+                        •
+                      </span>
+                    )}
+                    {discussion.comments.length > 0 && (
+                      <p className="text-muted-foreground hidden text-sm md:flex">
+                        Balasan terakhir{" "}
+                        {formatRelativeTime(discussion.comments[0].created_at)}
+                      </p>
+                    )}
+                  </div>
                 </div>
-              </div>
-            </CardContent>
-          </Card>
-        </Link>
-      ))}
+              </CardContent>
+            </Card>
+          </Link>
+        ))
+      )}
     </div>
   );
 }
